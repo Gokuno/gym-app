904b692226f3ec1b1e914d6df9c0f64f74f16ded
Allan Paz <jallanpazg@gmail.com>
2024-06-04 13:37:17 -0700

    Actualizacion de secciones y lenguaje

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,12 @@ export default function Hero() {
   return (
     <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[900px] w-full mx-auto p-4'>
         <div className='flex flex-col gap-4'>
-        <p>IT'S TIME TO GET</p>
-        <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Swole<span className='text-blue-400'>normous</span></h1>
+        <p>ES HORA DE COMENZAR</p>
+        <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Musclo<span className='text-blue-400'>sisimo</span></h1>
         </div>
-        <p className='text-sm md:text-base font-light'>I hereby acknowledge that I may become <span className='text-blue-400 font-medium'>unbelievably enormous</span> and accept all risks of becaming the local <span className='text-blue-400 font-medium'>mass monstrosity</span>, affected with severe body dismorphia, unable to fit through doors.</p>
+        <p className='text-sm md:text-base font-light'>Por la presente reconozco que puedo hacerme <span className='text-blue-400 font-medium'>increíblemente enorme</span> y aceptar todos los riesgos de convertirse en una <span className='text-blue-400 font-medium'>masa monstruosa</span> local, afectado con dismorfia corporal severa, incapaz de pasar por las puertas.</p>
         <button className='px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200'>
-           <p>Accept and Begin</p> 
+           <p>Aceptar y comenzar</p> 
         </button>
     </div>
   )
